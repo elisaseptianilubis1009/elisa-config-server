a961cdbbe7fdaf4d4ce1e187b17db39652a1d214
elisa1009 <elisa.septiani@powercommerce.asia>
2022-12-11 12:53:41 +0700

    init

---
+++ src/main/java/elisa/config/server/ElisaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package elisa.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElisaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElisaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elisa/config/server/ElisaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package elisa.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElisaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
