7b1a42a9e33c42c5ec3e60408dec36f76c008838
elisaseptianilubis1009 <elisaseptianilubis1009@gmail.com>
2022-12-11 19:21:35 +0700

    config

--- src/main/java/elisa/config/server/ElisaConfigServerApplication.java
+++ src/main/java/elisa/config/server/ElisaConfigServerApplication.java
@@ -2,8 +2,12 @@ package elisa.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class ElisaConfigServerApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +15,4 @@ public class ElisaConfigServerApplication {
 	}
 
 }
+
